5bbb461829078a2b61960627eb5809048cf3e75b
Jeemlei <eemeli.ingervo@gmail.com>
2021-01-29 19:36:02 +0200

    osa1 tehtävä 7 ja 8

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -12,9 +12,27 @@ const Button = ({ handler, text }) => {
   )
 }
 
-const Statistic = ({ text, amount }) => {
+const Statistic = ({ text, value }) => {
   return (
-    <p>{text} {amount}</p>
+    <p>{text} {value}</p>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = () => good + neutral + bad
+  const average = () => good / all() + (-1 * bad) / all()
+  const positive = () => (good / all() * 100) + " %"
+
+  return (
+    <div>
+      <Title text="statistics" />
+      <Statistic text="good" value={good} />
+      <Statistic text="neutral" value={neutral} />
+      <Statistic text="bad" value={bad} />
+      <Statistic text="all" value={all()} />
+      <Statistic text="average" value={average()} />
+      <Statistic text="positive" value={positive()} />
+    </div>
   )
 }
 
@@ -30,10 +48,7 @@ const App = () => {
       <Button handler={() => setGood(good + 1)} text="good" />
       <Button handler={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handler={() => setBad(bad + 1)} text="bad" />
-      <Title text="statistics" />
-      <Statistic text="good" amount={good} />
-      <Statistic text="neutral" amount={neutral} />
-      <Statistic text="bad" amount={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
