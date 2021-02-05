353a95bff7f8617b1984dead7f0abab9e5164d11
Jeemlei <eemeli.ingervo@gmail.com>
2021-02-05 03:12:03 +0200

    osa2 tehtävä 5

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,50 +1,5 @@
 import React from 'react'
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
-
-const Header = ({ course }) => {
-  return (
-    <div>
-      <h2>{course.name}</h2>
-    </div>
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(part =>
-        <Part key={part.id} part={part} />
-      )}
-    </div>
-  )
-}
-
-const Part = ({ part }) => {
-  return (
-    <div>
-      <p>
-        {part.name} {part.exercises}
-      </p>
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-  return (
-    <div>
-      <b>total of {parts.reduce((sum, part) => { return (sum + part.exercises) }, 0)} exercises</b>
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
