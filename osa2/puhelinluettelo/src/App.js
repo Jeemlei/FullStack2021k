1ab375886868d21b0f3c1bdc0f596e48d8a750f6
Jeemlei <eemeli.ingervo@gmail.com>
2021-02-06 20:22:44 +0200

    osa2 tehtävä 8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,39 +2,52 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      number: '040-1231244'
+    }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
 
     if (typeof persons.find(person => person.name === newName) === 'undefined') {
       const personObject = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
 
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     } else {
       alert(`${newName} is already added to phonebook`)
     }
   }
 
-  const handleNameInput = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleNameInput = (event) => setNewName(event.target.value)
+
+  const handleNumberInput = (event) => setNewNumber(event.target.value)
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name:
-          <input
-            value={newName}
-            onChange={handleNameInput}
-          />
+          <div>name:
+            <input
+              value={newName}
+              onChange={handleNameInput}
+            />
+          </div>
+          <div>number:
+            <input
+              value={newNumber}
+              onChange={handleNumberInput}
+            />
+          </div>
         </div>
         <div>
           <button type="submit">add</button>
@@ -43,7 +56,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map(person =>
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>{person.name} {person.number}</li>
         )}
       </ul>
     </div>
