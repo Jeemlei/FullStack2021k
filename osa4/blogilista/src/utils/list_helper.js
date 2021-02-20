1cad78ab794175ef3e416cd9ae6548721a22cd83
Jeemlei <eemeli.ingervo@gmail.com>
2021-02-20 20:04:20 +0200

    osa4 tehtävä 5

--- osa4/blogilista/src/utils/list_helper.js
+++ osa4/blogilista/src/utils/list_helper.js
@@ -1,11 +1,24 @@
 const dummy = blogs => 1
 
-const totalLikes = (blogs) => {
-    return blogs.reduce((sum, blog) => {
-        return sum + blog.likes
-    }, 0)
+const totalLikes = blogs => {
+	return blogs.reduce((sum, blog) => {
+		return sum + blog.likes
+	}, 0)
+}
+
+const favoriteBlog = blogs => {
+	const favorite = blogs.reduce((previous, current) => {
+		return previous.likes > current.likes ? previous : current
+	})
+	return {
+		title: favorite.title,
+		author: favorite.author,
+		likes: favorite.likes,
+	}
 }
 
 module.exports = {
-	dummy, totalLikes
+	dummy,
+	totalLikes,
+	favoriteBlog,
 }
