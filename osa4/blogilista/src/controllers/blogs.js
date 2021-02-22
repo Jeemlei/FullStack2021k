1191c230d14e3a87de222c4035807f1d9edd5893
Jeemlei <eemeli.ingervo@gmail.com>
2021-02-22 18:30:06 +0200

    osa4 tehtävät 17-19

--- osa4/blogilista/src/controllers/blogs.js
+++ osa4/blogilista/src/controllers/blogs.js
@@ -1,17 +1,45 @@
 const blogsRouter = require('express').Router()
-const { request, response } = require('express')
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
+const getTokenFrom = request => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+		return authorization.substring(7)
+	}
+	return null
+}
 
 blogsRouter.get('/', async (request, response) => {
-	const blogs = await Blog.find({})
+	const blogs = await Blog.find({}).populate('user')
 	response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-	const blog = new Blog(request.body)
+	const body = request.body
+	const token = getTokenFrom(request)
+	const decodedToken = jwt.verify(token, process.env.SECRET)
+	if(!token || !decodedToken.id) {
+		return response.status(401).json({ error: 'token missing or invalid' })
+	}
 
-	const result = await blog.save()
-	response.status(201).json(result)
+	const user = await User.findById(decodedToken.id)
+
+	const blog = new Blog({
+		title: body.title,
+		author: body.author,
+		url: body.url,
+		likes: body.likes,
+		user: user._id,
+	})
+
+	const savedBlog = await blog.save()
+
+	user.blogs = user.blogs.concat(savedBlog._id)
+	await user.save()
+
+	response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
@@ -29,7 +57,9 @@ blogsRouter.put('/:id', async (request, response) => {
 		likes: body.likes,
 	}
 
-	const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+	const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+		new: true,
+	})
 	response.json(updatedBlog)
 })
 
