9a237df42bd9156ff9689fe03c4572d4376ffe79
Jeemlei <eemeli.ingervo@gmail.com>
2021-03-01 00:19:18 +0200

    osa 5 tehtävä 11

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 import blogsService from '../services/blogs'
 
 const Blog = ({ blog, blogs, setBlogs }) => {
@@ -66,6 +67,12 @@ const Blog = ({ blog, blogs, setBlogs }) => {
 			</div>
 		</div>
 	)
+}
+
+Blog.propTypes = {
+	blog: PropTypes.object.isRequired,
+	blogs: PropTypes.array.isRequired,
+	setBlogs: PropTypes.func.isRequired,
 }
 
 export default Blog
