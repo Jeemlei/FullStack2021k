2b3ef65fab9c500f97b01bbe579d6fe18060e253
Jeemlei <eemeli.ingervo@gmail.com>
2021-05-26 19:05:05 +0300

    osa8 tehtävä 19

--- osa8/library-frontend/src/components/Books.js
+++ osa8/library-frontend/src/components/Books.js
@@ -5,10 +5,22 @@ import { ALL_BOOKS } from '../queries'
 const Books = props => {
 	const result = useQuery(ALL_BOOKS)
 	const [books, setBooks] = useState([])
+	const [genres, setGenres] = useState([])
+	const [genre, setGenre] = useState('')
 
 	useEffect(() => {
 		if (result.data) {
 			setBooks(result.data.allBooks)
+			setGenres(
+				result.data.allBooks.reduce((allGenres, b) => {
+					b.genres.forEach(booksGenre => {
+						if (!allGenres.includes(booksGenre)) {
+							allGenres = allGenres.concat(booksGenre)
+						}
+					})
+					return allGenres
+				}, [])
+			)
 		}
 	}, [result])
 
@@ -16,9 +28,20 @@ const Books = props => {
 		return null
 	}
 
+	let genreIndicator
+	if (genre) {
+		genreIndicator = (
+			<p>
+				in genre <b>{genre}</b>
+			</p>
+		)
+	}
+
 	return (
 		<div>
 			<h2>books</h2>
+
+			{genreIndicator}
 
 			<table>
 				<tbody>
@@ -27,15 +50,28 @@ const Books = props => {
 						<th>author</th>
 						<th>published</th>
 					</tr>
-					{books.map(b => (
-						<tr key={b.title}>
-							<td>{b.title}</td>
-							<td>{b.author.name}</td>
-							<td>{b.published}</td>
-						</tr>
-					))}
+					{books
+						.filter(b =>
+							b.genres.reduce(
+								(previous, g) => previous || g.includes(genre),
+								false
+							)
+						)
+						.map(b => (
+							<tr key={b.title}>
+								<td>{b.title}</td>
+								<td>{b.author.name}</td>
+								<td>{b.published}</td>
+							</tr>
+						))}
 				</tbody>
 			</table>
+			<div>
+				{genres.map(g => (
+					<button key={g} onClick={() => setGenre(g)}>{g}</button>
+				))}
+				<button onClick={() => setGenre('')}>all genres</button>
+			</div>
 		</div>
 	)
 }
