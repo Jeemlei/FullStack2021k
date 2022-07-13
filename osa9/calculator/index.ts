2996b1ae12a314ccbabffaf32ea95d686cffa6c0
Jeemlei <eemeli.ingervo@gmail.com>
2022-07-13 10:09:41 +0300

    osa9 tehtävä 5

--- osa9/calculator/index.ts
+++ osa9/calculator/index.ts
@@ -1,12 +1,33 @@
 import express from 'express'
+import calculateBmi from './bmiCalculator'
 
 const app = express()
 
 app.get('/hello', (_req, res) => {
-    res.send('Hello Full Stack!')
+	res.send('Hello Full Stack!')
 })
 
-const PORT = 3003
+app.get('/bmi', (req, res) => {
+	const height = Number(req.query.height)
+	const weight = Number(req.query.weight)
+
+	if (isNaN(height) || isNaN(weight)) {
+        res.status(400)
+		res.json({
+			error: 'malformatted parameters',
+		})
+	} else {
+		const bmi = calculateBmi(height, weight)
+
+		res.json({
+			weight,
+			height,
+			bmi,
+		})
+	}
+})
+
+const PORT = 3002
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
