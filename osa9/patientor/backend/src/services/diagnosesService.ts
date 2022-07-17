e54f238a376851526994f1786d526d2e0497ac94
Jeemlei <eemeli.ingervo@gmail.com>
2022-07-17 23:28:48 +0300

    osa9 tehtävä 10

---
+++ osa9/patientor/backend/src/services/diagnosesService.ts
@@ -0,0 +1,12 @@
+import diagnosesData from '../data/diagnoses.json';
+import { Diagnose } from '../types';
+
+const diagnoses: Array<Diagnose> = diagnosesData;
+
+const getDiagnoses = () => {
+    return diagnoses;
+};
+
+export default {
+    getDiagnoses
+};
