c518ea0e947eaa043ffd07d0aa2c4ef579ac957c
Jeemlei <eemeli.ingervo@gmail.com>
2022-12-22 22:18:50 +0200

    osa9 tehtävä 17

--- osa9/patientor/frontend/src/App.tsx
+++ osa9/patientor/frontend/src/App.tsx
@@ -9,6 +9,7 @@ import { Patient } from "./types";
 
 import PatientListPage from "./PatientListPage";
 import { Typography } from "@material-ui/core";
+import PatientPage from "./PatientPage";
 
 const App = () => {
   const [, dispatch] = useStateValue();
@@ -41,6 +42,9 @@ const App = () => {
           <Divider hidden />
           <Routes>
             <Route path="/" element={<PatientListPage />} />
+          </Routes>
+          <Routes>
+            <Route path="/patients/:id" element={<PatientPage />} />
           </Routes>
         </Container>
       </Router>
---
+++ osa9/patientor/frontend/src/PatientPage/index.tsx
@@ -0,0 +1,49 @@
+import { Typography } from '@material-ui/core';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { apiBaseUrl } from '../constants';
+import { useStateValue } from '../state';
+import { Gender, Patient } from '../types';
+import FemaleIcon from '@mui/icons-material/Female';
+import MaleIcon from '@mui/icons-material/Male';
+
+const PatientPage = () => {
+	const { id } = useParams<{ id: string }>();
+	const [state, dispatch] = useStateValue();
+	const [patient, setPatient] = useState<Patient | undefined>();
+	React.useEffect(() => {
+		if (id && (!state.patients[id] || !state.patients[id].ssn)) {
+			const fetchPatient = async () => {
+				try {
+					const { data: patientFromApi } = await axios.get<Patient>(
+						`${apiBaseUrl}/patients/${id}`
+					);
+					dispatch({ type: 'UPDATE_PATIENT', payload: patientFromApi });
+					setPatient(patientFromApi);
+				} catch (e) {
+					console.error(e);
+				}
+			};
+			void fetchPatient();
+		} else if (id) {
+			setPatient(state.patients[id]);
+		}
+	}, [dispatch]);
+
+	if (!patient) return <p>loading...</p>;
+
+	return (
+		<div style={{marginTop: '1rem'}}>
+			<Typography variant="h5">
+				{patient.name}{' '}
+				{(patient.gender === Gender.Female && <FemaleIcon />) ||
+					(patient.gender === Gender.Male && <MaleIcon />)}
+			</Typography>
+			<p style={{marginBottom: 0}}>ssn: {patient.ssn}</p>
+			<p style={{margin: 0}}>occupation: {patient.occupation}</p>
+		</div>
+	);
+};
+
+export default PatientPage;
