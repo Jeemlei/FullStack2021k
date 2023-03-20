7f343ce049de8842ba6ff77c50e38d534d8e14b7
Jeemlei <eemeli.ingervo@gmail.com>
2023-03-20 16:29:04 +0200

    osa10 tehtävä 8

--- osa10/rate-repository-app/src/components/SignIn.jsx
+++ osa10/rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,66 @@
 import Text from './Text'
+import { Formik } from 'formik'
+import FormikTextInput from './FormikTextInput'
+import { Pressable, StyleSheet, View } from 'react-native'
+import theme from '../theme'
+
+const styles = StyleSheet.create({
+	input: {
+		height: 50,
+		margin: 10,
+		paddingLeft: 15,
+		borderWidth: 1,
+		borderRadius: 5,
+	},
+	button: {
+		height: 50,
+		margin: 10,
+		marginTop: 5,
+		borderRadius: 5,
+		backgroundColor: theme.colors.primary,
+		color: theme.colors.secondary,
+		fontWeight: 'bold',
+		textAlign: 'center',
+		textAlignVertical: 'center',
+	},
+})
+
+const initialValues = {
+	username: '',
+	password: '',
+}
+
+const SignInForm = ({ onSubmit }) => {
+	return (
+		<View style={{ backgroundColor: 'white' }}>
+			<FormikTextInput
+				name="username"
+				placeholder="Username"
+				style={styles.input}
+			/>
+			<FormikTextInput
+				name="password"
+				placeholder="Password"
+				secureTextEntry={true}
+				style={styles.input}
+			/>
+			<Pressable onPress={onSubmit}>
+				<Text style={styles.button}>Sign in</Text>
+			</Pressable>
+		</View>
+	)
+}
 
 const SignIn = () => {
-	return <Text>The sign-in view</Text>
+	const onSubmit = values => {
+		console.log(values)
+	}
+
+	return (
+		<Formik initialValues={initialValues} onSubmit={onSubmit}>
+			{({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+		</Formik>
+	)
 }
 
 export default SignIn
