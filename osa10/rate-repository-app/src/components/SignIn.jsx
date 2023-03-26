aca1ac2a854c6203a6bac04d69759388621981f7
Jeemlei <eemeli.ingervo@gmail.com>
2023-03-26 14:24:10 +0300

    osa10 tehtävä 18

--- osa10/rate-repository-app/src/components/SignIn.jsx
+++ osa10/rate-repository-app/src/components/SignIn.jsx
@@ -47,6 +47,18 @@ const SignInForm = ({ onSubmit }) => {
 	)
 }
 
+export const SignInContainer = ({ onSubmit }) => {
+	return (
+		<Formik
+			initialValues={initialValues}
+			onSubmit={onSubmit}
+			validationSchema={validationSchema}
+		>
+			{({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+		</Formik>
+	)
+}
+
 const SignIn = () => {
 	const [signIn] = useSignIn()
 	const navigate = useNavigate()
@@ -63,15 +75,7 @@ const SignIn = () => {
 		}
 	}
 
-	return (
-		<Formik
-			initialValues={initialValues}
-			onSubmit={onSubmit}
-			validationSchema={validationSchema}
-		>
-			{({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
-		</Formik>
-	)
+	return <SignInContainer onSubmit={onSubmit} />
 }
 
 export default SignIn
